0e3abfebe598aec8e90142da1a07870528ef98de
athan <athandaviddev@gmail.com>
2015-06-20 11:58:29 +0300

    add sample project

---
+++ NSStringFormatWithNSArray/NSStringFormatWithNSArray/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSStringFormatWithNSArray
+//
+//  Created by david on 20/06/15.
+//  Copyright (c) 2015 Comunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
